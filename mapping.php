7e1e3b1d6b124c060ba8f867b45e35b797a65cf1
Ganesh Gupta <ogind-ggupta@users.noreply.github.com>
2023-07-13 08:12:38 -0500

    add mapping

--- mapping.php
+++ mapping.php
@@ -2,8 +2,23 @@
 
 $arrMapping = [
     "qty" => 'quantity',
-    "qtyn" => 'quantity',
-    'ship to:' => 'customer address'
-
-]
-    ?>
+    "qtyn" => 'quantity',    
+    'ship to:' => 'delivery address',
+    'delivery location:' => 'ship to',
+    'postal address:' => 'ship to',
+    'location:' => 'ship to',
+    'invoice:' => 'invoice number',
+    'invoice id:' => 'invoice number',
+    'invoice #' => 'invoice number',
+    'invoice number:' => 'invoice',
+    'invoice no:' => 'invoice',
+    'total' => 'invoice total',
+    'total due' => 'invoice total',
+    'p.o. number' => 'PO Number',
+    'name' => 'vendor name',
+    'due date' => 'scheduled payment',
+    'date:' => 'invoice date',
+    'vendor address' => 'address details',
+    'shipping & Handling' => 'Freight Delivery Charges'
+];
+?>

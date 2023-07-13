a6bbb53a144598a1bf122f4226ec770de22b639e
shantanuk90 <skulkarni@opengov.com>
2023-07-14 00:28:13 +0530

    updated mappings

--- mapping.php
+++ mapping.php
@@ -1,38 +1,68 @@
 <?php
 
-$arrMapping = [
-    "qty" => 'quantity',
-    "qtyn" => 'quantity',
-    'quantity' => 'quantity received',    
-    'ship to:' => 'delivery address',
-    'ship to' => 'delivery address',
-    'delivery location:' => 'ship to',
-    'postal address:' => 'ship to',
-    'location:' => 'ship to',
+$arrMapping = [  
+    //invoice number
+    'invoice no.' => 'invoice number',
     'invoice:' => 'invoice number',
-    'invoice id:' => 'invoice number',
-    'invoice #' => 'invoice number',
-    'invoice number:' => 'invoice',
-    'invoice no:' => 'invoice',
     'invoice' => 'invoice number',
+    'invoice id' => 'invoice number',
+    'invoice#' => 'invoice number',
+    'invoice number' => 'invoice number',
+    'invoice no:' => 'invoice number',
+    //vendor number
+    'customer id' => 'vendor number',
+    'cust:' => 'vendor number',
+    //vendor details
+    'name' => 'vendor name',
+    'street' => 'address details',
+    'city' => 'city',
+    'state' => 'state',
+    'zip_code' => 'zip code',
+    //invoice total amount
     'total' => 'invoice total',
     'total due' => 'invoice total',
-    'p.o. number' => 'PO Number',
-    'cust. p. o.' => 'PO Number',
-    'name' => 'vendor name',
-    'due date' => 'scheduled payment',
-    'final due date' => 'scheduled payment',
-    'date:' => 'invoice date',
+    //invoice date
+    'date' => 'invoice date',
     'invoice date' => 'invoice date',
-    'Order\ndate' => 'invoice date',
-    'vendor address' => 'address details',
-    'Remit to' => 'address details',
-    'shipping & Handling' => 'Freight Delivery Charges',
-    'cust:' => 'customer number',
-    'tax exemption' => 'tax payer id',
-    'amount' => 'detail amount',
-    'description' => 'item description',
-    'T' => 'phone number',
+    'date:' => 'invoice date',
+    'order\ndate' => 'invoice date',
+    //scheduled payment
+    'final due date' => 'scheduled payment',
+    'please pay invoice by' => 'scheduled payment',
+    'due date' => 'scheduled payment',
+    //PO number
+    'p.o. no' => 'po number',
+    'p.o. number' => 'po number',
+    'cust. p.o.' => 'po number',
+    //delivery charges
+    'shipping' => 'delivery charges',
+    'shipping & handling' => 'delivery charges',
+    //Line Item details
+    'total' => 'detail amount',
+    'amount' => 'detail amount',    
     'subtotal' => 'detail amount',
-]
+    'description' => 'item description',
+    'items' => 'item description',
+    //quantity received
+    'qty' => 'quantity',
+    'qtyn' => 'quantity',
+    'quantity received' => 'quantity',
+    //delivery address
+    'ship to:' => 'delivery address',
+    'ship to' => 'delivery address',
+    'location:' => 'delivery address',
+    'Remit to' => 'delivery address',
+    'tax exemption' => 'tax payer id',   
+    't' => 'phone number', 
+];
+
+$removeKeys = [
+    'RECEIVER_SHIP_TO',
+    'RECEIVER_ADDRESS',
+    'RECEIVER_NAME',
+    'RECEIVER_PHONE',
+    'VENDOR_REMIT_TO',
+    'RECEIVER',
+];
+
 ?>

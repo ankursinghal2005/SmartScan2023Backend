13e1ce4126f4ffd443f8eff494459ed30ac171c5
Ganesh Gupta <ogind-ggupta@users.noreply.github.com>
2023-07-13 02:51:10 -0500

    update aws sdk file

--- textract.php
+++ textract.php
@@ -3,6 +3,11 @@
 require 'vendor/autoload.php'; // Make sure to include the AWS SDK for PHP
 
 use Aws\Textract\TextractClient;
+use Aws\Comprehend\ComprehendClient;
+use Aws\Exception\AwsException;
+
+include_once('mapping.php');
+
 // Amazon S3 API credentials 
 
 class smartScan{
@@ -14,7 +19,13 @@ class smartScan{
     private$secret_access_key = 'HOZkAn+LcX6eLKTLo6XpqOAr+hy8gKC17GlEvxtn'; 
     private $bucket = 'oghackathon23'; 
 
+    private $schemaMapping = [];
+
     private $client = '';
+
+    private $comprehendClient = "";
+
+    private $arrMapping = [];
 
     function __construct(){
         putenv("AWS_ACCESS_KEY_ID=" . $this->access_key_id);
@@ -29,6 +40,55 @@ class smartScan{
         ];
 
         $this->client = new TextractClient($credentials);
+
+        $this->comprehendClient = new ComprehendClient([
+                                    'region' => $this->region,
+                                    'version' => $this->version, // Update with the desired version
+                                ]);
+
+        $this->schemaMapping();                        
+    }
+
+    public function setMapping($arrMapping)
+    {
+        $this->arrMapping = $arrMapping;
+    }
+
+    private function schemaMapping(){
+
+        $this->schemaMapping = [
+            'Date' => 'https://schema.org/DateTime',
+            'Amount' => 'https://schema.org/PriceSpecification',
+            'Currency' => 'https://schema.org/Text',
+            'Invoice' => 'https://schema.org/Invoice',
+            'Payment' => 'https://schema.org/PaymentChargeSpecification',
+            'Account' => 'https://schema.org/BankAccount',
+            'Transaction' => 'https://schema.org/FinancialTransaction',
+            'Tax' => 'https://schema.org/Tax',
+            'Vendor' => 'https://schema.org/Organization',
+            'Customer' => 'https://schema.org/Person',
+            'Product' => 'https://schema.org/Product',
+            'Place' => 'https://schema.org/Place',
+            'Image' => 'https://schema.org/ImageObject',
+            'Video' => 'https://schema.org/VideoObject',
+            'Audio' => 'https://schema.org/AudioObject',
+            'Text' => 'https://schema.org/Text',
+            'URL' => 'https://schema.org/URL',
+            'Supplier' => 'https://schema.org/Organization',
+            'Vendor' => 'https://schema.org/Organization',
+            'Manufacturer' => 'https://schema.org/Organization',
+            'LogisticsCompany' => 'https://schema.org/Organization',
+            'DeliveryMethod' => 'https://schema.org/Text',
+            'Shipping' => 'https://schema.org/ParcelDelivery',
+            'Freight' => 'https://schema.org/ParcelDelivery',
+            'Incoterms' => 'https://schema.org/Text',
+            'Customs' => 'https://schema.org/GovernmentOrganization',
+            'Invoice Date' => 'https://schema.org/DateTime',
+            'Total Amount' => 'https://schema.org/MonetaryAmount',
+            'Customer Name' => 'https://schema.org/Person',
+            'Shipping Address' => 'https://schema.org/PostalAddress',
+            // Add more categories as needed
+        ];
     }
     public function analyzeDocument($doc){
         $s3Object = [
@@ -47,6 +107,7 @@ class smartScan{
     public function getExpenseRecord(&$result){
         
         $arrFinalData = [];
+        $arrLebel = [];
         if(empty($result['ExpenseDocuments']) || empty($result['ExpenseDocuments'][0]) || empty($result['ExpenseDocuments'][0]['SummaryFields']) ){
             return $arrFinalData;
         }
@@ -69,16 +130,67 @@ class smartScan{
             if(!empty($summaryFields['ValueDetection'])){
                 $ValueDetection = $summaryFields['ValueDetection']['Text']; 
             }
-            
-            $arrData = ['Label' => $LabelDetection,'value'=> $ValueDetection];
+
+            $arrTmp = ['Label' => $LabelDetection, 'value' => $ValueDetection];
+            $LabelDetection = strtolower($LabelDetection);
+            if (!empty($this->arrMapping[$LabelDetection])) {
+                $arrTmp['meta_tag'] = $this->arrMapping[$LabelDetection];
+            }
+            if (in_array($LabelDetection, $this->arrMapping)) {
+                $arrTmp['meta_tag'] = $LabelDetection;
+            }
+
+
+            $this->detectEntities($LabelDetection, $arrTmp);
+
             if(!empty($summaryFields['GroupProperties'][0]['Types'])){
                 $GroupProperties = $summaryFields['GroupProperties'][0]['Types'][0]; 
-                $arrFinalData[$GroupProperties][$Type] = $arrData;
+                $arrFinalData[$GroupProperties][$Type] = $arrTmp;
             }else{
-                $arrFinalData[$Type] = $arrData;
+                $arrFinalData[$Type] = $arrTmp;
             }
         }
+
+        
         return $arrFinalData;
+    }
+
+    private function detectEntities($label,&$arrTmp)
+    {
+        try {
+            $response = $this->comprehendClient->detectEntities([
+                'LanguageCode' => 'en',
+                'Text' => $label,
+            ]);
+
+            $response = json_decode(json_encode($response), true);
+            $enrichedLabel = [
+                'Label' => $label,
+                'Category' => 'Unknown',
+                'Confidence' => 0.0,
+            ];
+
+            if (!empty($response['Entities'])) {
+                $entity = max($response['Entities'], function ($a, $b) {
+                    return $a['Score'] <=> $b['Score'];
+                });
+
+                $category = $this->schemaMapping[$label] ?? 'Unknown';
+
+                $enrichedLabel = [
+                    'Label' => $label,
+                    'Category' => $category,
+                    'Confidence' => $entity['Score'],
+                ];
+            }
+
+            $arrTmp['enriched_label'] = $enrichedLabel;
+          
+
+        } catch (AwsException $e) {
+            echo 'Error: ' . $e->getAwsErrorMessage() . PHP_EOL;
+            return null;
+        }
     }
 
     public function getLineItemDetails(&$result){
@@ -111,8 +223,17 @@ class smartScan{
                     if(!empty($LineItem['ValueDetection'])){
                         $ValueDetection = $LineItem['ValueDetection']['Text']; 
                     }
-                    
-                    $arrData[] = ['Label' => $LabelDetection,'value'=> $ValueDetection];
+
+                    $arrTmp = ['Label' => $LabelDetection, 'value' => $ValueDetection];
+                    $LabelDetection = strtolower($LabelDetection);
+                    if (!empty($this->arrMapping[$LabelDetection])) {
+                        $arrTmp['meta_tag'] = $this->arrMapping[$LabelDetection];
+                    }
+                    if (in_array($LabelDetection, $this->arrMapping)) {
+                        $arrTmp['meta_tag'] = $LabelDetection;
+                    }
+
+                    $arrData[] = $arrTmp;
                     
                 }
              
@@ -126,17 +247,29 @@ class smartScan{
 
 try{
 
+    
     header('Content-Type: application/json; charset=utf-8');
     $arrRes = ['status' => 'fail','data'=>[]];
     if(empty($_GET['file_name'])){
-        $arrRes['msg'] = "file_name is missing !";
-        echo json_encode($arrRes); exit();
+    $arrRes['msg'] = "file_name is missing !";
+    echo json_encode($arrRes); exit();
     };
     
     
     $objSmartScan = new smartScan();
     $result = $objSmartScan->analyzeDocument($_GET['file_name']);
     $result =  (array) $result;
+    
+
+    /*
+    // for test purpose only
+    $objSmartScan = new smartScan();
+    $result = file_get_contents('result.json');
+    $result = json_decode($result, true);
+    */
+
+
+    $objSmartScan->setMapping($arrMapping);
 
     if(empty($result) || !is_array($result) ){
         $arrRes['msg'] = "Un able to parse the document !";
